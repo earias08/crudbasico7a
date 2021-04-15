4f3222e9eff60011ec3545a5624694c3cf970da5
Emilse <emilsearias@gmail.com>
2021-04-15 18:57:14 -0300

    se agrego el archivo db.json y json server

--- src/components/AgregarProducto.js
+++ src/components/AgregarProducto.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Container, Form} from 'react-bootstrap';
 
 const AgregarProducto = () => {
     return (
-        <div>
-            <h1>Pagina Agregar Producto</h1>
-        </div>
+       <Container>
+           <Form className='my-5'>
+               <h1 className='text-center my-5'>Agregar nuevo producto</h1>
+               <Form.Group>
+                   <Form.Label>Nombre de producto*</Form.Label>
+                   <Form.Control type='text' placeholder='Té'></Form.Control>
+               </Form.Group>
+               <Form.Group>
+                   <Form.Label>Precio*</Form.Label>
+                   <Form.Control type='number' placeholder='0'></Form.Control>
+               </Form.Group>
+               <h3 className='text-center my-4'>Categoria</h3>
+               <div className='text-center'>
+                   <Form.Check type='radio' name='categoria' inline label='Bebida caliente'></Form.Check>
+                   <Form.Check type='radio' name='categoria' inline label='Bebida Fria'></Form.Check>
+                   <Form.Check type='radio' name='categoria' inline label='Dulce'></Form.Check>
+                   <Form.Check type='radio' name='categoria' inline label='Salado'></Form.Check>
+               </div>
+           </Form>
+       </Container>
     );
 };
 
